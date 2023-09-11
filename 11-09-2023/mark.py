b3476e3b7be21eea0bc222b2f3253020bd8c20ae
Ajaykumar <144038390+Akajay0708@users.noreply.github.com>
2023-09-11 22:03:42 +0530

    Add files via upload

---
+++ 11-09-2023/mark.py
@@ -0,0 +1,12 @@
+name="Ajaykumar"
+address="vembadithalam,salem"
+mark="12th mark sheet"
+tamil=70
+english=80
+accountancy=80
+econamic=80
+commarce=70
+computer=80
+total_mark=(tamil+english+accountancy+econamic+commarce+computer)
+average=(total_mark/600)
+print("NAME:",name ,"Address:",address,"Mark:",mark ,"total_mark:",total_mark,"average:",average)
